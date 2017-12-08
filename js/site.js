409f3095e9a0d5c52295774b721949713db0d74c
TylerTomasello <ttomasel@hawk.iit.edu>
2017-12-07 18:04:43 -0600

    Fix slideshow to work with images

--- js/site.js
+++ js/site.js
@@ -1,18 +1,16 @@
-$.noConflict();
-
-(function($) {
-
-  $(document).ready(function(){
-
-    $(function(){
-      $('.slideshow img:gt(0)').hide();
-      setInterval(function(){
-        $('.slideshow :first-child').hide()
-          .next('img').fadeIn(1000)
-          .end().appendTo('.slideshow');},
-      5500);
-    });
-
-  });
-
-})(jQuery);
+//Javascript for making the images a slideshow
+slideshow();
+function slideshow() {
+  var index = 0;
+  var i = 0;
+  var x = document.getElementsByClassName("imgslide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  index++;
+  if (index > x.length) {
+    index = 1;
+  }
+  x[index-1].style.display = "block";
+  setTimeout(slideshow, 5000);  //change every 5 seconds
+}
